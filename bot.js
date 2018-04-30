c5cec10d8c1b89127bfc6d4a0265ffa6f57b232f
Intex000 <33152349+Intex000@users.noreply.github.com>
2018-04-30 20:48:54 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,72 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const config = require("./config.json");
+const PREFIX ="sv.";
+
+
+// Log in
+bot.on('ready', async () => {
+  console.log("CodePurple is ready! ", (bot.user.tag));  
+  bot.user.setActivity("Prefix: sv.", {type: "playing"});
+});
+// Commands
+bot.on("message", function(message) {
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray;
+  if(message.content === "sv.kk002"){
+  bot.channel.get(433130096287678464).sendMessage("HELLO LOL bye");
+  }
+
+
+if (!message.content.startsWith(PREFIX)) return;  
+var args = message.content.substring(PREFIX.length).split(" ");
+var print = console.log;
+let prefix = config.prefix;
+
+let args2 = messageArray.slice(1);
+let owner = message.guild.roles.find("name", "Owner") 
+let admin = message.guild.roles.find("name", "Admin")
+let moderator = message.guild.roles.find("name", "Moderator")   
+switch (args[0]) {
+  case "discord":
+    message.channel.sendMessage(config.discordlink);
+    print((config.thread), "A User just executed a command: sv.discord")
+    break;
+
+  case "ip":
+    message.channel.sendMessage("The IP is: ")
+    print((config.thread), "A User just executed a command: sv.ip")
+    break;
+  case ":":
+  if(!message.member.hasPermission("ADMINISTRATOR")) return;
+      const sayMessage = args.join(" ");
+      message.delete().catch();
+      message.channel.send(sayMessage);
+  case "randomnumber":
+    var randomNumb = Math.random();
+    message.channel.send((randomNumb));
+  break;  
+  case "cmds":
+  var embed = new Discord.RichEmbed()
+  .setTitle("Please use sv.help ")
+  message.channel.sendMessage(embed)
+  break;
+  case "help":
+
+    var embed = new Discord.RichEmbed()
+    .setTitle("Commands List!")   
+    .setDescription(config.help)
+    .setFooter("The bot prefix is: sv.")
+    .setColor("993")
+    message.reply("Check your DMs")
+    message.author.sendEmbed(embed);
+      console.log((config.thread), "A User just executed a command: sv.help")
+    break;
+}
+if(message.content === s)
+  let logs = new Discord.RichEmbed()
+  .setDescription("")
+  .setColor("#00cc00")  
+  //new
+});
+bot.login(process.env.BOT_TOKEN);
