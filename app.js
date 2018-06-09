4228e5dd27f2b99bee746682d97ca0726fd1a362
A99 <33152349+Intex000@users.noreply.github.com>
2018-06-09 12:28:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ bot.on("message", async message => {
   
   if(message.content === "$gg") {
     let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[1]));
+    if(!kUser) message.channel.sendMessage("Error: cannot find user")
   message.channel.sendMessage('meow meow' + (kUser) + 'Hello');
   }
   
