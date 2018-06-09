e23706fbc629389676afde084e779bfa720fdc35
A99 <33152349+Intex000@users.noreply.github.com>
2018-06-09 11:11:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ switch (args[0]) {
   case "ip":
     message.channel.sendMessage("The IP is: SlingShotV.aternos.me \nVersion: 1.11x");
     print((message.author.username), "issued bot command: ip");
-    
+    message.guild.channels.find('name', "¤》logs").send((message.author.username) + " issued bot command: discord");
 
     break;
   case ":":
@@ -44,18 +44,18 @@ switch (args[0]) {
       const sayMessage = args.join(" ");
       message.delete().catch();
       message.channel.send(sayMessage);
-    message.guild.channels.find('name', "logs").sendMessage((message.author.username) + " issued bot command: :");
+    message.guild.channels.find('name', "¤》logs").sendMessage((message.author.username) + " issued bot command: :");
     break;
   case "randomnumber":
     var randomNumb = Math.random();
     message.channel.send((randomNumb));
-    message.guild.channels.find('name', "logs").sendMessage((message.author.username) + " issued bot command: randomnumber");
+    message.guild.channels.find('name', "¤》logs").sendMessage((message.author.username) + " issued bot command: randomnumber");
   break;  
   case "cmds":
   var embed = new Discord.RichEmbed()
   .setTitle("Please use $help ")
   message.channel.sendMessage(embed)
-    message.guild.channels.find('name', "logs").sendMessage((message.author.username) + " issued bot command: cmds");
+    message.guild.channels.find('name', "¤》logs").sendMessage((message.author.username) + " issued bot command: cmds");
   break;
   case "test":
     
@@ -69,7 +69,7 @@ switch (args[0]) {
     message.reply("Check your DMs")
     message.author.sendEmbed(embed);
       console.log((message.author.username), "issued bot command: help");
-    message.guild.channels.find('name', "logs").sendMessage((message.author.username) + " issued bot command: help");
+    message.guild.channels.find('name', "¤》logs").sendMessage((message.author.username) + " issued bot command: help");
     break;
 }
 });
